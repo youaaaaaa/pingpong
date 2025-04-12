5d4b78a70fa2417db53f8a9bacf2db5091e09128
youaaaaaa <kira.eshengoma@gmail.com>
2025-04-12 12:37:16 +0300

    Create main.py
    классы GameSprite, Player
    одна ракетка (слева), обработка движения вверх-вниз

---
+++ main.py
@@ -0,0 +1,95 @@
+from pygame import *
+from pygame import sprite
+from random import randint
+from time import time as timer
+
+window_size_x = 700
+window_size_y = 500
+player_size_x = 50
+player_size_y = 100
+score = 0 # сбито
+lost = 0 # пропущено
+bullets = sprite.Group()
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x,size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.size_x = size_x
+        self.size_y = size_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+     
+    def goLeft(self):
+        if self.rect.x >= self.speed:
+            self.rect.x -= self.speed
+
+    def goRight(self):
+        if self.rect.x < window_size_x - self.size_x and self.rect.x < window_size_x - self.speed:
+            self.rect.x += self.speed
+
+    def goUp(self):
+        if self.rect.y >= self.speed:
+            self.rect.y -= self.speed
+
+    def goDown(self):
+        if self.rect.y < window_size_y - self.size_y and self.rect.y < window_size_y - self.speed:
+            self.rect.y += self.speed
+
+
+class Player (GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP]:
+            super().goUp()
+        if keys_pressed[K_DOWN]:
+            super().goDown()
+      
+
+window = display.set_mode((700,500))
+
+display.set_caption("PingPong")
+
+background = transform.scale(image.load("table.jpg"),(700,500))
+
+FPS = 60
+clock = time.Clock()
+game=True
+
+mixer.init()
+#mixer.music.load('space.ogg')
+#mixer.music.play()
+
+font.init()
+font2 = font.SysFont('Arial', 36)
+font1 = font.SysFont('Arial', 72)
+
+
+player = Player("puddle.jpg",50, 250, 50, 100, 10)
+
+
+finish=False
+
+while game:
+
+    for e in event.get():
+        if e.type==QUIT:
+            game=False
+
+    if not finish:
+        window.blit(background,(0,0))
+        player.update()
+
+        player.reset()
+    
+    display.update()
+
+    time.delay(50)
+    
+    clock.tick(FPS)
